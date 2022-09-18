6123cede411f89a4d818f013a7188127bffc08fe
HiImZsofi <zsofia.gyori00@gmail.com>
2022-09-18 12:02:39 +0200

    get private integers

--- app/src/main/java/com/example/colorpicker/MainActivity.java
+++ app/src/main/java/com/example/colorpicker/MainActivity.java
@@ -3,8 +3,19 @@ package com.example.colorpicker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.slider.Slider;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Slider slider1;
+    private Slider slider2;
+    private Slider slider3;
+    private TextView tw1;
+    private TextView tw2;
+    private TextView tw3;
+    private TextView tw4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
