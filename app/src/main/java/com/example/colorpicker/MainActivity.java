3d215303cfda70a99b6c2573702636adec3e8077
HiImZsofi <zsofia.gyori00@gmail.com>
2022-09-19 10:41:00 +0200

    first slider setup

--- app/src/main/java/com/example/colorpicker/MainActivity.java
+++ app/src/main/java/com/example/colorpicker/MainActivity.java
@@ -3,7 +3,9 @@ package com.example.colorpicker;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.google.android.material.slider.Slider;
@@ -17,22 +19,24 @@ public class MainActivity extends AppCompatActivity {
     private TextView tw2;
     private TextView tw3;
     private TextView tw4;
-    public float value;
+    private RelativeLayout mainLayout;
+    private int redValue;
+    private int greenValue;
+    private int blueValue;
+    private RelativeLayout coloredLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-        slider1.addOnSliderTouchListener(new Slider.OnSliderTouchListener() {
+        slider1.addOnChangeListener(new Slider.OnChangeListener() {
             @Override
-            public void onStartTrackingTouch(@NonNull Slider slider) {
-                value = slider.getValue();
-            }
-
-            @Override
-            public void onStopTrackingTouch(@NonNull Slider slider) {
-                tw1.setText((int) value);
+            public void onValueChange(@NonNull Slider slider, float value, boolean fromUser) {
+                redValue = (int) slider1.getValue();
+                tw1.setText("Piros: " + redValue);
+                tw4.setText(colorToString(redValue, greenValue, blueValue));
+                coloredLayout.setBackgroundColor(Color.rgb(redValue, greenValue, blueValue));
             }
         });
     }
@@ -46,5 +50,11 @@ public class MainActivity extends AppCompatActivity {
         tw2 = findViewById(R.id.tw2);
         tw3 = findViewById(R.id.tw3);
         tw4 = findViewById(R.id.tw4);
+        mainLayout = findViewById(R.id.mainLayout);
+        coloredLayout = findViewById(R.id.coloredLayout);
+    }
+
+    private String colorToString(int r, int g, int b) {
+        return "(" + r + "," + g + "," + b + ")";
     }
 }
